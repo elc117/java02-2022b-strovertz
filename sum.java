58d3f9c6236cb2f7c971a41c705b04c35153767c
Strovertz - Gleison Pires <74078237+strovertz@users.noreply.github.com>
2022-09-27 11:54:10 -0300

    atualizando codigo

--- sum.java
+++ sum.java
@@ -3,11 +3,21 @@ import java.util.Collections;
 import java.util.Arrays;
 
 class roupas {
+    public static void main(String[] args) {
+        String[] top = {"vermelho", "azul", "verde", "rosa"};
 
-    String[] top = {"vermelho", "azul", "verde", "rosa"};
+        String[] down = {"vermelho", "azul", "verde", "rosa"};
 
-    String[] down = {""};
+        String[] socks = {"vermelho", "azul", "verde", "rosa"};
 
-    String[] socks = {};
+        Random generator = new Random();
 
+        int randomindex = generator.nextInt(top.length);
+        int randomindex2 = generator.nextInt(down.length);
+        int randomindex3 = generator.nextInt(socks.length);
+
+        System.out.println("Segue lista de roupas que você deve sair hoje");
+        System.out.println("Camiseta: " + top[randomindex] + "Calça: " + down[randomindex2] + "Meias: " + socks[randomindex3] + "!");
+
+    }
 }
