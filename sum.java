b5e4cadffb7569c8b0dd7f4c4737459eb3de4ff5
Strovertz - Gleison Pires <74078237+strovertz@users.noreply.github.com>
2022-09-27 11:46:14 -0300

    atualizando ideia

---
+++ sum.java
@@ -0,0 +1,13 @@
+import java.util.Random;
+import java.util.Collections;
+import java.util.Arrays;
+
+class roupas {
+
+    String[] top = {"vermelho", "azul", "verde", "rosa"};
+
+    String[] down = {""};
+
+    String[] socks = {};
+
+}
