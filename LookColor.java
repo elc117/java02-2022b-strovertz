c4e31cfa05b4b78a0edac8c22f79aee907395849
Strovertz - Gleison Pires <74078237+strovertz@users.noreply.github.com>
2022-09-27 11:59:46 -0300

    atualizando arquivos

---
+++ LookColor.java
@@ -0,0 +1,26 @@
+import java.util.Random;
+
+/*
+ * O Programa vai gerar cores aleatorias para combinar 
+ * as roupas que o usuário ira vestir no dia
+ * 
+ */
+class LookColor {
+    public static void main(String[] args) {
+        String[] top = {"vermelho", "azul", "verde", "rosa"};
+
+        String[] down = {"vermelho", "azul", "verde", "rosa"};
+
+        String[] socks = {"vermelho", "azul", "verde", "rosa"};
+
+        Random generator = new Random();
+
+        int randomindex = generator.nextInt(top.length);
+        int randomindex2 = generator.nextInt(down.length);
+        int randomindex3 = generator.nextInt(socks.length);
+
+        System.out.println("Segue lista de roupas que você deve sair hoje");
+        System.out.println("Camiseta: " + top[randomindex] + "Calça: " + down[randomindex2] + "Meias: " + socks[randomindex3] + "!");
+
+    }
+}
